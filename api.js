0fccf7a1bfd0e0f9251550778541489e7ab06d56
Kevin Sawicki <kevinsawicki@gmail.com>
2016-05-11 17:09:03 +0200

    Log install location

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const electron = require('electron')
 
 exports.install = () => {
+  console.log(`Installing Devtron from ${__dirname}`)
   electron.remote.BrowserWindow.addDevToolsExtension(__dirname)
 }
 
