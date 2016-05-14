7c914d5669c2a70756ddc4653064254e908ec9b9
Steve Kinney <hello@stevekinney.net>
2016-05-14 13:07:56 -0400

    Add error message if triggered from the main process

--- api.js
+++ api.js
@@ -1,6 +1,9 @@
 const electron = require('electron')
 
 exports.install = () => {
+  if (!electron.remote) {
+    throw new Error('Devtron cannot be installed from within the main process.')
+  }
   console.log(`Installing Devtron from ${__dirname}`)
   return electron.remote.BrowserWindow.addDevToolsExtension(__dirname)
 }
