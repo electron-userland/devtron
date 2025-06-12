4433c07334e3c0d837c85226676414e8ebba84b6
hitarth-gg <goforhitarth@gmail.com>
2025-06-13 00:35:09 +0530

    chore: fixed IPC tracking in renderer

--- externals/electron-renderer-tracker.js
+++ externals/electron-renderer-tracker.js
@@ -2,26 +2,13 @@ import { ipcRenderer } from 'electron';
 import { MSG_TYPE } from '../common/constants';
 
 export function monitorRenderer() {
-  const originalSend = ipcRenderer.send.bind(ipcRenderer);
-  const originalInvoke = ipcRenderer.invoke.bind(ipcRenderer);
-  const originalSendSync = ipcRenderer.sendSync.bind(ipcRenderer);
+  ipcRenderer.on(MSG_TYPE.RENDER_EVENT, (event, data) => {
+    track('renderer-to-main', data.channel, data.args);
+  });
+
   const originalOn = ipcRenderer.on.bind(ipcRenderer);
   const originalOnce = ipcRenderer.once.bind(ipcRenderer);
-
-  ipcRenderer.send = (channel, ...args) => {
-    track('renderer-to-main', channel, args);
-    return originalSend(channel, ...args);
-  };
-
-  ipcRenderer.invoke = (channel, ...args) => {
-    track('renderer-to-main', channel, args);
-    return originalInvoke(channel, ...args);
-  };
-
-  ipcRenderer.sendSync = (channel, ...args) => {
-    track('renderer-to-main', channel, args);
-    return originalSendSync(channel, ...args);
-  };
+  const originalAddListener = ipcRenderer.addListener.bind(ipcRenderer);
 
   ipcRenderer.on = (channel, listener) => {
     return originalOn(channel, (_event, ...args) => {
@@ -32,6 +19,13 @@ export function monitorRenderer() {
 
   ipcRenderer.once = (channel, listener) => {
     return originalOnce(channel, (_event, ...args) => {
+      track('main-to-renderer', channel, args);
+      listener(_event, ...args);
+    });
+  };
+
+  ipcRenderer.addListener = (channel, listener) => {
+    return originalAddListener(channel, (_event, ...args) => {
       track('main-to-renderer', channel, args);
       listener(_event, ...args);
     });
